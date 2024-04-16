60298e15e07bf8fe2f7b2c109de8e2103a6c3071
ExequielSilvestre <exequiel.silvestre@scalar.io>
2024-04-16 18:16:38 -0300

    Adding cloudinary for managing images

---
+++ Backend/config/CloudinaryConfig.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: 'ddjhhlt7w',
+  api_key: '564968177958343',
+  api_secret: 'bfsxCOrdYNns5uKzvtEfREM47Vo'
+});
+
+module.exports = cloudinary;
